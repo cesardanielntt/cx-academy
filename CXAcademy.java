88dda79560aafea5913a617a4af5f87598e7cc04
César <cesar.daniel@outlook.com.br>
2021-07-07 18:16:27 -0300

    adicionei minha classe CXAcademy

---
+++ CXAcademy.java
@@ -0,0 +1,18 @@
+public class CXAcademy
+{
+
+	public static void main(final String[] args)
+	{
+		System.out.println("Main")
+	}
+
+	public void make()
+	{
+		System.out.println("make something")
+	}
+
+	public void add()
+	{
+		System.out.println("add something")
+	}
+}

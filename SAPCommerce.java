3d7977610f25abfa522af01f78687fc647011fe6
unknown <thalesgc2011@hotmail.com>
2021-07-13 19:11:57 -0300

    Testando o git

--- SAPCommerce.java
+++ SAPCommerce.java
@@ -1,11 +1,10 @@
 public class SAPCommerce {
 
- public void addToCart() {
-System.out.println("Adicionando item ao carrinho...");
-}
+	public void addToCart() {
+		System.out.println("Adiciona item ao carrinho");
 
- public void removeItemFromCart() {
-System.out.println("Remove item do carrinho...");
-}
-
+	}
+	public void removeItemFromCart() {
+		System.out.println("Remove item do carrinho");
+	}
 }

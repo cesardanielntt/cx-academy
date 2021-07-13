83b96ff3f115b9eb03f69cef48d1c11060332cd6
alvesmaiara <alvessmaiara22@gmail.com>
2021-07-13 10:28:22 -0300

    Implementa classe SAP commerce

---
+++ SAPCommerce.java
@@ -0,0 +1,12 @@
+public class SAPCommerce {
+
+	public void addToCart () {
+		System.out.println("Adiciona item ao carrinho");
+    }
+
+	public void removeItemFromCart () {
+		System.out.println ("Remove item do carrinho");
+    }
+
+
+}

2f51b731a48b03953c314d9d570782cc135f80a1
ramonsilva2 <ramonsilva.silva.rsrs@gmail.com>
2021-07-13 10:38:30 -0300

    resolvendo conflitos

--- SAPCommerce.java
+++ SAPCommerce.java
@@ -1,15 +1,15 @@
 public class SAPCommerce {
   
 	public void addToCart() {
-		System.out.println("Adiciona item ao carrinho");
+		System.out.println("Adiciona item ao carrinho.");
 	}
 
 	public void removeItemFromCart() {
-		System.out.println("Remove item do carrinho");
+		System.out.println("Remove item do carrinho.");
 	}
 	
 	public void updateItemFromCart(){
-		System.out.println("Atualiza item do carrinho");
+		System.out.println("Atualiza item do carrinho.");
 	}
 
 }

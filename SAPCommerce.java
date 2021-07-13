bf578546039d484bd8cd3249a69ba75f8f14a1a6
Leandro <leandro.r327@gmail.com>
2021-07-13 10:27:37 -0300

    Adicionando metodo

--- SAPCommerce.java
+++ SAPCommerce.java
@@ -7,5 +7,9 @@ public class SAPCommerce {
     public void removeItemFromCart() {
         System.out.println("Remove item do carrinho");
     }
+
+    public void updateItemFromCart() {
+        System.out.println("Atualiza item do carrinho");
+    }
  
 }

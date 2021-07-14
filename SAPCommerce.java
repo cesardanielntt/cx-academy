e831b8115a135998d2e8130b8ce32a0b2096cfdf
sakurapath <leandro.canestraro050@gmail.com>
2021-07-14 12:09:34 -0300

    correcting SAPCommerce class

--- SAPCommerce.java
+++ SAPCommerce.java
@@ -1,11 +1,15 @@
 public class SAPCommerce {
 
- public void addToCart() {
-System.out.println("Adicionando item ao carrinho...");
-}
+    public void addToCart() {
+        System.out.println("Adicionando item ao carrinho...");
+    }
 
- public void removeItemFromCart() {
-System.out.println("Remove item do carrinho...");
-}
+    public void removeItemFromCart() {
+        System.out.println("Removendo item do carrinho...");
+    }
 
+     public void updateItemFromCart() {
+        System.out.println("Atualizando item do carrinho...");
+
+     }
 }

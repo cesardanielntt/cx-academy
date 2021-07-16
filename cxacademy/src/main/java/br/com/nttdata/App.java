48444a19a0d0139a373beff412f250ee8db3e096
sakurapath <leandro.canestraro050@gmail.com>
2021-07-16 12:31:30 -0300

    Adding classes and methods

--- cxacademy/src/main/java/br/com/nttdata/App.java
+++ cxacademy/src/main/java/br/com/nttdata/App.java
@@ -1,7 +1,58 @@
 package br.com.nttdata;
 
+import br.com.nttdata.models.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main (String[] args) {
-        
+
+
+        Pessoa person = new Pessoa("Leandro");
+        person.setLastName("Rowling");
+        person.setEmail("teste@gmail.com");
+        person.setAge(19);
+        System.out.println(person.toString());
+
+        Pessoa person2 = new Pessoa("Lucas");
+        person2.setLastName("Vinular");
+        person2.setEmail("teste2@gmail.com");
+        person2.setAge(51);
+        System.out.println(person2.toString());
+
+
+
+    }
+
+    public static double sumNumbers(double a, double b){
+        return a+b;
+    }
+
+    public static void forLoops(){
+        String nome = "Leandro";
+        String lastName = "Canestraro";
+
+
+        List<String> arrayNum = new ArrayList<>();
+        arrayNum.add(nome);
+        arrayNum.add(lastName);
+
+        for(int i=0; i<arrayNum.size(); i++){
+            System.out.println(arrayNum.get(i));
+        }
+
+        for (String item: arrayNum) {
+            System.out.println(item);
+        }
+
+        for(int i=0; i<100; i++) {
+            boolean value = i%2 == 0;
+            if (value){
+                System.out.println(i+" is even");
+            } else {
+                System.out.println(i+" is odd");
+            }
+        }
     }
 }
---
+++ cxacademy/src/main/java/br/com/nttdata/models/Pessoa.java
@@ -0,0 +1,54 @@
+package br.com.nttdata.models;
+
+public class Pessoa {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public Pessoa(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}

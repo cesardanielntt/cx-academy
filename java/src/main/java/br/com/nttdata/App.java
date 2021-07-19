3a745a31b5510d77f1ddee13297b82cc3b65fa32
SkyesOnFire <40586787+SkyesOnFire@users.noreply.github.com>
2021-07-19 11:43:12 -0300

    implementation of register.jsp

--- java/src/main/java/br/com/nttdata/App.java
+++ java/src/main/java/br/com/nttdata/App.java
@@ -1,29 +1,46 @@
 package br.com.nttdata;
 import br.com.nttdata.models.Pessoa;
+import br.com.nttdata.models.PessoaFisica;
+import br.com.nttdata.models.PessoaJuridica;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
     final static String meuNome = "Teilor";
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Teilor");
+
+        PessoaFisica pessoa = new PessoaFisica();
+        pessoa.setNome("Teilor");
         pessoa.setSobrenome("Miotto");
         pessoa.setEmail("teilor_miotto@hotmail.com");
         pessoa.setIdade(24);
         System.out.println(pessoa.toString());
 
-        Pessoa pessoa2 = new Pessoa("Cleber");
-        pessoa2.setSobrenome("Teste");
+        PessoaFisica pessoa2 = new PessoaFisica();
+        pessoa2.setNome("Teste");
+        pessoa2.setSobrenome("Sobrenome");
         pessoa2.setEmail("teste@teste.com");
         pessoa2.setIdade(99);
         System.out.println(pessoa2.toString());
 
+        PessoaJuridica pj1 = new PessoaJuridica();
+        pj1.setNome("Pessoa Juridica");
+        pj1.setEmail("pj@teste.com");
+        pj1.setCnpj("01.467.985/0001-45");
+        System.out.println(pj1.toString());
+
         List<Pessoa> pessoas = new ArrayList<>();
         pessoas.add(pessoa);
         pessoas.add(pessoa2);
+        pessoas.add(pj1);
 
         for (Pessoa item : pessoas){
-            System.out.println(item.getNome());
+            if(item instanceof PessoaFisica){
+                System.out.println(((PessoaFisica) item).getSobrenome());
+            }else if(item instanceof PessoaJuridica){
+                System.out.println(((PessoaJuridica) item).getCnpj());
+            }
         }
 
         //fors();
---
+++ java/src/main/webapp/WEB-INF/jsp/register.jsp
@@ -0,0 +1,38 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: teilor.miotto
+  Date: 19/07/2021
+  Time: 10:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Pagina de Cadastro</h1>
+
+<form action="" method="post">
+    <div class="row">
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" placeholder="Digite seu nome"/>
+    </div>
+    <div class="row">
+        <label for="sobrenome">Sobrenome</label>
+        <input type="text" name="sobrenome" placeholder="Digite seu sobrenome"/>
+    </div>
+    <div class="row">
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Digite seu email"/>
+    </div>
+    <div class="row">
+        <label for="idade">Idade</label>
+        <input type="number" name="idade" placeholder="Digite sua idade"/>
+    </div>
+
+    <button type="submit">Enviar</button>
+</form>
+
+</body>
+</html>

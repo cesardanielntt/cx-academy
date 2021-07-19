9eb1ad4ecbc6825998bd651719ad3a228c5b5474
Karoline <karol.penteadoalves@gmail.com>
2021-07-19 11:55:15 -0300

    alterações

---
+++ java/src/main/java/br/com/nttdata/models/PessoaFisica.java
@@ -0,0 +1,27 @@
+package br.com.nttdata.models;
+
+public class PessoaFisica extends Pessoa {
+    private String sobrenome;
+    private int idade;
+
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String nomeCompleto(){
+        return this.nome+" "+this.sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}

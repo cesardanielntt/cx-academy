42aa9c896eaba7f8d6034ab33d2a24ff0b19f9e8
sakurapath <leandro.canestraro050@gmail.com>
2021-07-20 10:26:11 -0300

    .jsp fixes and new Controller

--- cxacademy/src/main/java/br/com/nttdata/controllers/PessoaCadastroContoller.java
+++ cxacademy/src/main/java/br/com/nttdata/controllers/PessoaCadastroContoller.java
@@ -1,11 +1,16 @@
 package br.com.nttdata.controllers;
 
 import br.com.nttdata.data.CadastroForm;
+import br.com.nttdata.models.PessoaFisica;
 import br.com.nttdata.service.impl.PessoaFisicaService;
 import org.glassfish.jersey.server.mvc.Viewable;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
 
 @Path("register")
 
@@ -21,11 +26,22 @@ public class PessoaCadastroContoller {
     }
 
     @POST
-    public String register(@BeanParam CadastroForm form) {
+    public Response register(@BeanParam CadastroForm form) throws URISyntaxException {
 
         pessoaService = new PessoaFisicaService();
-        System.out.println(form.getName());
-        return "Funcionou";
+        PessoaFisica pessoaFisica = new PessoaFisica();
+
+        UUID id = UUID.randomUUID();
+
+        pessoaFisica.setId(id.toString());
+        pessoaFisica.setName(form.getName());
+        pessoaFisica.setLastName(form.getLastName());
+        pessoaFisica.setEmail(form.getEmail());
+        pessoaFisica.setAge(form.getAge());
+
+        pessoaService.criarPessoa(pessoaFisica);
+
+        return Response.seeOther(new URI("pessoas")).build();
     }
 
 }

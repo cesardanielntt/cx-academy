ade1d18f4915f41eea35a62c3952359cf84b7924
tiago <tiago.schlogl@hotmail.com>
2021-07-20 13:54:05 -0300

    Iniciando cadastro de pessoas.

--- java/src/main/java/br/com/nttdata/controllers/PessoaCadastroController.java
+++ java/src/main/java/br/com/nttdata/controllers/PessoaCadastroController.java
@@ -2,13 +2,22 @@ package br.com.nttdata.controllers;
 
 
 import br.com.nttdata.data.CadastroForm;
+import br.com.nttdata.models.PessoaFisica;
+import br.com.nttdata.service.PessoaService;
+import br.com.nttdata.service.impl.PessoaFisicaService;
 import org.glassfish.jersey.server.mvc.Viewable;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
 
 @Path("register")
 public class PessoaCadastroController {
+
+    private PessoaService pessoaService;
 
     @GET
     @Produces(MediaType.TEXT_HTML)
@@ -17,9 +26,24 @@ public class PessoaCadastroController {
 
     }
     @POST
-    public String register(@BeanParam CadastroForm form){
+    public Response register(@BeanParam CadastroForm form) throws URISyntaxException {
+        pessoaService = new PessoaFisicaService();
+
+        PessoaFisica pessoaFisica = new PessoaFisica();
+
+        String id = UUID.randomUUID().toString();
+
+        pessoaFisica.setId(id);
+        pessoaFisica.setNome(form.getNome());
+        pessoaFisica.setSobrenome(form.getSobrenome());
+        pessoaFisica.setIdade(form.getIdade());
+        pessoaFisica.setEmail(form.getEmail());
+
+        pessoaService.criarPessoa(pessoaFisica);
+
         System.out.println(form.getNome());
-        return "Funcionou";
+        Response pessoas = Response.seeOther(new URI("pessoas")).build();
+        return pessoas;
         }
 
 }

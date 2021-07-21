84b98a4a2ac5a508aa249f42f2fa076a99297a1c
Pedro <pedro_rique17@hotmail.com>
2021-07-21 08:40:28 -0300

    criacao classe endereco

--- java/src/main/java/br/com/nttdata/models/Endereco.java
+++ java/src/main/java/br/com/nttdata/models/Endereco.java
@@ -1,12 +1,46 @@
 package br.com.nttdata.models;
 
 public class Endereco {
+
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
     private String cidade;
     private String estado;
-    private String bairro;
-    private String rua;
     private String cep;
-    private String numero;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 
     public String getCidade() {
         return cidade;
@@ -24,35 +58,11 @@ public class Endereco {
         this.estado = estado;
     }
 
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
     public String getCep() {
         return cep;
     }
 
     public void setCep(String cep) {
         this.cep = cep;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
     }
 }

5b121e84633dd51bab7068c7591e83d39d0873bc
tiago <tiago.schlogl@hotmail.com>
2021-07-26 12:31:55 -0300

    atividades spring

---
+++ Spring/tiago/src/main/java/com/nttdata/spring/cxacademy/dao/ProductDao.java
@@ -0,0 +1,13 @@
+package com.nttdata.spring.cxacademy.dao;
+
+import com.nttdata.spring.cxacademy.model.ProductModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductDao extends CrudRepository<ProductModel,Integer> {
+
+    List<ProductModel> findAll();
+}

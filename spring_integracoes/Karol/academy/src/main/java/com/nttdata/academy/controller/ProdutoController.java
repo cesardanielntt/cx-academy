9550904a59bfb61744af2aba4b2a3ca00699773b
Karollays <karol.penteadoalves@gmail.com>
2021-07-27 12:40:43 -0300

    arquivos da aula 27/07

---
+++ spring_integracoes/Karol/academy/src/main/java/com/nttdata/academy/controller/ProdutoController.java
@@ -0,0 +1,26 @@
+package com.nttdata.academy.controller;
+
+
+import com.nttdata.academy.dto.ProdutoDTO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    private static final Logger LOG = LogManager.getLogger(ProdutoController.class);
+
+    @RequestMapping(value = "/adicionar", method = RequestMethod.POST)
+    public ResponseEntity adicionarProduto(@RequestBody ProdutoDTO cliente){
+
+        LOG.debug(cliente.toString());
+
+        return ResponseEntity.ok().body(cliente);
+    }
+}

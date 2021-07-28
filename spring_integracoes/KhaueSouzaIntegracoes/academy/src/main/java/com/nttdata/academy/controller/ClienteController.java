d4a52689ef4130cf94ce43974c0e391c1af90564
Khaue-Souza <khaue.souza@hotmail.com>
2021-07-27 23:53:35 -0300

    Atividade de spring

---
+++ spring_integracoes/KhaueSouzaIntegracoes/academy/src/main/java/com/nttdata/academy/controller/ClienteController.java
@@ -0,0 +1,37 @@
+package com.nttdata.academy.controller;
+
+
+import com.nttdata.academy.dto.ClienteDTO;
+import com.nttdata.academy.facade.ClienteFacade;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+        /**executar as execuções e mapear a propria classe */
+    private static final Logger LOG = LogManager.getLogger(ClienteController.class);
+
+    @Resource(name = "clienteFacade")
+    private ClienteFacade clienteFacade;
+
+    @RequestMapping(value = "/adicionar", method = RequestMethod.POST)
+    public ResponseEntity adicionarCliente(@RequestBody ClienteDTO cliente){
+
+
+        LOG.debug(cliente.toString());
+        clienteFacade.adicionar(cliente);
+
+        return ResponseEntity.ok().body(cliente);
+    }
+
+
+}
---
+++ spring_integracoes/KhaueSouzaIntegracoes/academy/src/main/java/com/nttdata/academy/facade/impl/DefaultClienteFacade.java
@@ -0,0 +1,8 @@
+package com.nttdata.academy.facade.impl;
+
+
+import org.springframework.stereotype.Service;
+
+@Service("clienteFacade")
+public class DefaultClienteFacade {
+}

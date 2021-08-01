73538fc0b493930e5ee2b9797add8bec03b87e58
Khaue-Souza <khaue.souza@hotmail.com>
2021-08-01 16:37:37 -0300

    Front-feito

--- aula_front/Khaue_boodstrap/js/script.js
+++ aula_front/Khaue_boodstrap/js/script.js
@@ -1,45 +1,47 @@
-$('.js-check-zipcode').on('keyup', function(e) {
-	const $this = $(e.currentTarget);
-	const zipCode = $this.val();
 
-	if(zipCode.length === 8) {
-		const getAddress = getAddressByZipCode(zipCode);
+$('.js-check-zipcode').on('keyup', function (e) {
+    const $this = $(e.currentTarget);
+    const zipCode = $this.val();
 
-		getAddress.done(function(result){
-			if(!result.erro) {
-				const streetField = $('.js-complete-street');
-				const districtField = $('.js-complete-district');
-				const cityField = $('.js-complete-city');
-				const stateField = $('.js-complete-state');
+    if (zipCode.length === 8) {
+        const getAddress = getAddressByZipCode(zipCode);
 
-				if (result.bairro !== "") {
-					districtField.val(result.bairro);
-				}
+        getAddress.done(function (result) {
+            if (!result.erro) {
+                const streetField = $('.js-complete-street');
+                    const districtField = $('.js-complete-district');
+                    const cityField = $('.js-complete-city');
+                    const stateField = $('.js-complete-state');
 
-				if (result.logradouro !== "") {
-					streetField.val(result.logradouro);
-				}
+                if (result.logradouro !== "") {
+                    streetField.val(result.logradouro);
+                }
+                if (result.bairro !== "") {
+                    districtField.val(result.bairro);
+                }
 
-				if (result.localidade !== "") {
-					cityField.val(result.localidade);
-				}
+                if (result.cidade !== "") {
+                    cityField.val(result.localidade);
+                }
+                if (result.uf !== "") {
+                    stateField.val(result.uf);
+                }
+                $this.val(result.cep);
+            }
+        });
 
-				if (result.uf !== "") {
-					stateField.val(result.uf);
-				}
+        
+    }
 
-				$this.val(result.cep);
-			}
-		});
-	}
+
 });
 
-
 function getAddressByZipCode(userZipCode) {
-	const requestURL = `https://viacep.com.br/ws/${userZipCode}/json/`;
+    const requestURL = `https://viacep.com.br/ws/${userZipCode}/json/`;
 
-	return $.ajax(requestURL, {
-		method: 'GET',
-		dataType: 'json'
-	});
+    return $.ajax(requestURL, {
+        method: 'GET',
+        dataType: 'json'
+    });
+
 }

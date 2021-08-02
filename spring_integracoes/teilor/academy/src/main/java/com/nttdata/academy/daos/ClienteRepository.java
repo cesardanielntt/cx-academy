070c5c34dc69b6cf9786369df1e6df6363835fcd
SkyesOnFire <40586787+SkyesOnFire@users.noreply.github.com>
2021-08-02 00:56:16 -0300

    Client services

---
+++ spring_integracoes/teilor/academy/src/main/java/com/nttdata/academy/daos/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.nttdata.academy.daos;
+
+import com.nttdata.academy.models.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteModel, Integer> {
+
+    Object findAllById(Integer id);
+}
